303a06326d50317342209efdcfa185cfc403fd28
Achyutha Bharadwaj <32027107+AchyuthaBharadwaj@users.noreply.github.com>
2019-04-01 01:30:30 -0700

    shuffle logic

--- src/components/globals/Bars.js
+++ src/components/globals/Bars.js
@@ -207,6 +207,33 @@ export default class Bars extends Component {
       },
     ],
   }
+
+  shuffle = array => {
+    var currentIndex = array.length
+    var temporaryValue, randomIndex
+
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    }
+
+    return array
+  }
+
+  toggle = () => {
+    const temp = this.state.bars.slice()
+    this.shuffle(temp)
+    this.setState({
+      bars: temp,
+    })
+  }
+
   render() {
     return (
       <BarsWrapper>
@@ -219,6 +246,7 @@ export default class Bars extends Component {
                 background: item.color,
                 height: item.height,
               }}
+              onClick={this.toggle}
             />
           )
         })}
