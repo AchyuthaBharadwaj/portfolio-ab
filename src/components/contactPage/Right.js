ad987077f56a4a70d4236aeb0841d960c87c7dd4
Achyutha Bharadwaj <32027107+AchyuthaBharadwaj@users.noreply.github.com>
2019-04-08 05:48:32 -0700

    contact form netlify 3

--- src/components/contactPage/Right.js
+++ src/components/contactPage/Right.js
@@ -9,13 +9,15 @@ export default function Right({ className }) {
         Or fill in your details below and wait for me to contact you.
       </p>
       <div className="lineBreak" />
-      {/* <form
+      <form
         className="form"
         action="/thanks/"
-        method="POST"
         name="Contact"
-        netlify
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
       >
+        <input type="hidden" name="bot-field" />
         <label for="full-name" className="text" style={{ marginTop: "0" }}>
           Full name*
         </label>
@@ -40,34 +42,6 @@ export default function Right({ className }) {
           <p className="message">*All fields are mandatory</p>
           <input className="submit" type="submit" value="Ok, Send!" />{" "}
         </div>
-      </form> */}
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="bot-field" />
-        <div className="field half first">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
-        </div>
-        <div className="field half">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
-        </div>
-        <div className="field">
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" rows="6" />
-        </div>
-        <ul className="actions">
-          <li>
-            <input type="submit" value="Send Message" className="special" />
-          </li>
-          <li>
-            <input type="reset" value="Clear" />
-          </li>
-        </ul>
       </form>
     </RightWrapper>
   )
